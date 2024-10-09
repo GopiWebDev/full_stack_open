caf4b77da7f9b7916181ffc0f725563d54d3637d
GopiWebDev <gopisv.webdev@gmail.com>
2024-10-09 17:19:49 +0530

    completed 4.5

--- part_4/blog_app/utils/list_helper.js
+++ part_4/blog_app/utils/list_helper.js
@@ -1,12 +1,23 @@
-const dummy = (blogs) => {
-  return 1
-}
-
 const totalLikes = (blogs) => {
   return blogs ? blogs.reduce((prev, curr) => curr.likes + prev, 0) : 0
 }
 
+const favoriteBlog = (blogs) => {
+  let max = blogs[0]
+  for (let i = 0; i < blogs.length; i++) {
+    if (max.likes < blogs[i].likes) {
+      max = blogs[i]
+    }
+  }
+
+  return {
+    title: max.title,
+    author: max.author,
+    likes: max.likes,
+  }
+}
+
 module.exports = {
-  dummy,
   totalLikes,
+  favoriteBlog,
 }
