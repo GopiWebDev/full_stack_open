c61bbdcf5ab5e1f87e33f5efdb897946b859481b
GopiWebDev <gopisv.webdev@gmail.com>
2024-10-09 16:09:20 +0530

    completed 4.2

--- part_4/blog_app/app.js
+++ part_4/blog_app/app.js
@@ -3,8 +3,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -17,7 +22,11 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
