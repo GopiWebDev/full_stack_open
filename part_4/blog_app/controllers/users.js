1f33a46521cd2178621ddfabe5b354a096d78df4
GopiWebDev <gopisv.webdev@gmail.com>
2024-10-25 22:33:22 +0530

    completed 4.16

--- part_4/blog_app/controllers/users.js
+++ part_4/blog_app/controllers/users.js
@@ -2,21 +2,30 @@ const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (req, res) => {
-  const { username, name, password } = req.body
+usersRouter.post('/', async (request, response, next) => {
+  try {
+    const { username, name, password } = request.body
+    if (password.length < 3) {
+      return response
+        .status(400)
+        .json({ error: 'password length must be atleast 3 characters long' })
+    }
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
 
-  const savedUser = await user.save()
+    const savedUser = await user.save()
 
-  res.status(201).json(savedUser)
+    response.status(201).json(savedUser)
+  } catch (error) {
+    next(error)
+  }
 })
 
 usersRouter.get('/', async (request, response) => {
