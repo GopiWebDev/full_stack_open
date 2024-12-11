a9a2d9d203dae740d95e85e7a9bcda0679dc4a0d
GopiWebDev <gopisv.webdev@gmail.com>
2024-12-11 21:36:31 +0530

    completed 9.5 - 9.6

--- part_9/exercises/exerciseCalculator.ts
+++ part_9/exercises/exerciseCalculator.ts
@@ -1,59 +1,59 @@
-import isNotNumber from './utils'
+// import isNotNumber from './utils';
 
 interface Result {
-  periodLength: number
-  trainingDays: number
-  success: boolean
-  rating: number
-  ratingDescription: string
-  target: number
-  average: number
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
-const parseArgs = (args: string[]) => {
-  if (args.length < 8) throw new Error('Not enough args')
+// const parseArgs = (args: string[]) => {
+//   if (args.length < 8) throw new Error('Not enough args');
 
-  let daysArray = []
-  let start = 3
+//   const daysArray = [];
+//   let start = 3;
 
-  while (args[start]) {
-    daysArray.push(Number(args[start]))
-    start++
-  }
+//   while (args[start]) {
+//     daysArray.push(Number(args[start]));
+//     start++;
+//   }
 
-  let allNumbers = true
-  daysArray.map((day) => {
-    if (isNotNumber(day)) allNumbers = false
-  })
+//   let allNumbers = true;
+//   daysArray.map((day) => {
+//     if (isNotNumber(day)) allNumbers = false;
+//   });
 
-  if (allNumbers) {
-    return {
-      days: daysArray,
-      target: Number(args[2]),
-    }
-  } else {
-    throw new Error('Provided values were not numbers')
-  }
-}
+//   if (allNumbers) {
+//     return {
+//       days: daysArray,
+//       target: Number(args[2]),
+//     };
+//   } else {
+//     throw new Error('Provided values were not numbers');
+//   }
+// };
 
-const calculateExercises = (days: number[], target: number): Result => {
-  const periodLength = days.length
+export const calculateExercises = (days: number[], target: number): Result => {
+  const periodLength = days.length;
 
-  let trainingDays = 0
-  days.map((day) => (day > 0 ? (trainingDays += 1) : 0))
+  let trainingDays = 0;
+  days.map((day) => (day > 0 ? (trainingDays += 1) : 0));
 
-  let success = true
-  days.map((day) => (day < target ? (success = false) : success))
+  let success = true;
+  days.map((day) => (day < target ? (success = false) : success));
 
-  let avg = 3 / periodLength
-  let rating = Math.ceil(trainingDays * avg)
+  const avg = 3 / periodLength;
+  const rating = Math.ceil(trainingDays * avg);
 
-  let ratingDescription = 'bad'
-  if (rating === 2) ratingDescription = 'not too bad but could be better'
-  else if (rating === 3) ratingDescription = 'good target achieved'
+  let ratingDescription = 'bad';
+  if (rating === 2) ratingDescription = 'not too bad but could be better';
+  else if (rating === 3) ratingDescription = 'good target achieved';
 
   const average =
-    days.reduce((prev: number, curr: number) => (prev += curr)) / periodLength
+    days.reduce((prev: number, curr: number) => (prev += curr)) / periodLength;
 
   return {
     periodLength,
@@ -63,17 +63,5 @@ const calculateExercises = (days: number[], target: number): Result => {
     ratingDescription,
     target,
     average,
-  }
-}
-
-try {
-  const { days, target } = parseArgs(process.argv)
-
-  console.log(calculateExercises(days, target))
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message
-  }
-  console.log(errorMessage)
-}
+  };
+};
--- part_9/exercises/index.ts
+++ part_9/exercises/index.ts
@@ -1,13 +1,57 @@
-import express from 'express'
+import express, { Response, Request } from 'express';
+import calculateBmi from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
+import { calculator, Operator } from './calculator';
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!')
-})
+app.get('/bmi', (req: Request, res: Response): any => {
+  const { height, weight } = req.query;
 
-const PORT = 3000
+  const heightNum = Number(height);
+  const weightNum = Number(weight);
+
+  if (
+    Number.isNaN(heightNum) ||
+    Number.isNaN(weightNum) ||
+    heightNum <= 0 ||
+    weightNum <= 0
+  ) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  const result = calculateBmi(heightNum, weightNum);
+
+  if (result === 'Invalid') {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+
+  res.status(200).json({
+    weight,
+    height,
+    bmi: result,
+  });
+});
+
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  const result = calculator(Number(value1), Number(value2), op as Operator);
+  res.send({ result });
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  const result = calculateExercises(daily_exercises, Number(target));
+  res.send({ result });
+});
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
