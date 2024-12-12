5db92d21458551edb6c9c2e090220bd940c6fc90
GopiWebDev <gopisv.webdev@gmail.com>
2024-12-12 15:56:29 +0530

    completed 9.9

--- part_9/patientor/backend/index.ts
+++ part_9/patientor/backend/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('Pong');
 });
