d25766eff34eb08920a9012f389e8ac389a99b8c
GopiWebDev <gopisv.webdev@gmail.com>
2024-12-16 13:23:44 +0530

    completed 9.24

--- part_9/patientor/frontend/src/components/SinglePatient.tsx
+++ part_9/patientor/frontend/src/components/SinglePatient.tsx
@@ -21,6 +21,27 @@ const SinglePatient = () => {
       <p>gender: {patient.gender}</p>
       <p>ssh: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
+      <div>
+        <h3>Entries</h3>
+        {patient.entries &&
+          patient.entries.map((p) => {
+            return (
+              <div key={p.date}>
+                <p>
+                  {p.date}
+                  {'  '}
+                  {p.description}
+                </p>
+                <ul>
+                  {p.diagnosisCodes &&
+                    p.diagnosisCodes.map((code) => {
+                      return <li key={code}>{code}</li>;
+                    })}
+                </ul>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
